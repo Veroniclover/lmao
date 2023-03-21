a0c0a73775b51c8f23aa2e9858f4000a246649aa
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-03-21 17:06:48 +0900

    fix: exit -> break

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ i=0
 while [[ "$((i+=1))" -le "${posting}" ]]; do
     bash fblowres.sh "${1}" || { : "$((rep_its+=1))" ; continue ;}
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
-    [[ "${rep_its}" -ge 5 ]] && { echo "Failed 5 times" ; exit 0 ;}
+    [[ "${rep_its}" -ge 5 ]] && { echo "Failed 5 times" ; break ;}
 done
 # Update about
 total_archived="$(wc -l < log.txt) posts has been successfully backed up from March 2023 until now"
