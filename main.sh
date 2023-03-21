e4d851d68ce11bd3cc3dd2b32589534c2fd521c6
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-03-21 11:00:37 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,3 +6,6 @@ while [[ "$((i+=1))" -le "${posting}" ]]; do
     bash fblowres.sh "${1}" || { true ; continue ;}
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
 done
+# Update about
+total_archived="almost $(wc -l < log.txt) has been successfully from March 21, 2023 until now"
+curl -sLk -X POST "https://graph.facebook.com/me/?about=${total_archived}&access_token=${1}" || true
