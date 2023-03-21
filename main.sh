70a4f1cc695045d2be5a41ef2ff6512135533b33
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-03-21 17:02:38 +0900

    fix: terrible grammar and randomizer

--- main.sh
+++ main.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-posting=10
+posting="$(shuf -n 1 -i 8-12)"
 i=0
 while [[ "$((i+=1))" -le "${posting}" ]]; do
-    bash fblowres.sh "${1}" || { true ; continue ;}
+    bash fblowres.sh "${1}" || { : "$((rep_its+=1))" ; continue ;}
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
+    [[ "${rep_its}" -ge 5 ]] && { echo "Failed 5 times" ; exit 0 ;}
 done
 # Update about
-total_archived="$(wc -l < log.txt) posts has been successfully from March 2023 until now"
+total_archived="$(wc -l < log.txt) posts has been successfully backed up from March 2023 until now"
 curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${total_archived}" || true
