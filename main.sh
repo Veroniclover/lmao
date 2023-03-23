77bf8a27d1e563c53787e0df010acc1cef134eb7
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-03-23 13:09:42 +0900

    error handling (#4)
    * error handling
    
    * fix: image not dl

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@
 posting="$(shuf -n 1 -i 8-12)"
 i=0
 while [[ "$((i+=1))" -le "${posting}" ]]; do
-    bash fblowres.sh "${1}" || { : "$((rep_its+=1))" ; continue ;}
+    bash fblowres.sh "${1}" || { : "$((rep_its+=1))" ; echo "error: retrying" ; continue ;}
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
     [[ "${rep_its}" -ge 5 ]] && { echo "Failed 5 times" ; break ;}
 done
