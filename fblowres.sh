80d7bae52bdc2273500c2ab43ba8dd38a57337c4
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-03-26 00:21:28 +0800

    fix: log (#5)
    * fix: log
    
    * chore: add git_tok

--- fblowres.sh
+++ fblowres.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 token="${1}"
+git_tok="${2}"
 graph_url_main="https://graph.facebook.com"
+fetch_gist_base="https://gist.githubusercontent.com/Veroniclover/226f8ed0960e64fc43f6c3aae4cadbe7/raw/myfile"
+
+exit_custom(){
+	if [ -n "${post_id}" ]; then
+		fetch_gist_tofile+=$'\n'"Failed: ${post_id}"
+		printf '%s' "${fetch_gist_tofile}" | jq --raw-input --slurp '{files: {myfile: {content: .}}}' | curl -X PATCH -sL "https://api.github.com/gists/226f8ed0960e64fc43f6c3aae4cadbe7" -H 'Accept: application/vnd.github.v3+json' -H "Authorization: token ${git_tok}" --data @-
+	fi
+	exit 1
+}
 
 com_commenter(){
 	while IFS= read -r comnts; do
@@ -25,17 +35,17 @@ com_commenter(){
 		)"
 		sleep 10
 		if [ -n "${imglnk}" ]; then
-			curl -sLf "${imglnk}" -o com_tmp.jpg || exit 1
+			curl -sLf "${imglnk}" -o com_tmp.jpg || exit_custom
 			curl -sLf -X POST \
 				-F "message=${capt_compose}" \
 				-F "source=@com_tmp.jpg" \
 				-o /dev/null \
-			"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || exit 1
+			"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || exit_custom
 		else
 			curl -sLf -X POST \
 				--data-urlencode "message=${capt_compose}" \
 				-o /dev/null \
-		"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || exit 1
+		"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || exit_custom
 		fi
 	done <<-EOF
 	${status_footer}
@@ -69,10 +79,10 @@ post_to_timeline(){
 	
 	# dl files
 	if [ -n "${thumbnail}" ]; then
-		curl -sLf "${thumbnail}" -o thumb.jpg || exit 1
+		curl -sLf "${thumbnail}" -o thumb.jpg || exit_custom
 	fi
 	if [ -n "${vid_link}" ]; then
-		curl -sLf "${vid_link}" -o vid.mp4 || exit 1
+		curl -sLf "${vid_link}" -o vid.mp4 || exit_custom
 	fi
 	# upload now
 	if [ -n "${vid_link}" ]; then
@@ -80,17 +90,17 @@ post_to_timeline(){
 			-F "access_token=${token}" \
 			-F "source=@vid.mp4" \
 			-F "description=${capt_compose}" \
-		"${graph_url_main}/v16.0/me/videos")" || exit 1
+		"${graph_url_main}/v16.0/me/videos")" || exit_custom
 		id_post="$(printf '%s' "${id_post}" | sed -nE 's|.*id":"([^"]*)".*|\1|p')"
-		[ -z "${id_post}" ] && exit 1
+		[ -z "${id_post}" ] && exit_custom
 	else
 		id_post="$(curl -sLf -X POST \
 			-F "access_token=${token}" \
 			-F "source=@thumb.jpg" \
 			-F "message=${capt_compose}" \
-		"${graph_url_main}/v16.0/me/photos")" || exit 1
+		"${graph_url_main}/v16.0/me/photos")" || exit_custom
 		id_post="$(printf '%s' "${id_post}" | sed -nE 's|.*id":"([^"]*)".*|\1|p')"
-		[ -z "${id_post}" ] && exit 1
+		[ -z "${id_post}" ] && exit_custom
 	fi
 	# comment another infos
 	sleep 10
@@ -99,12 +109,12 @@ post_to_timeline(){
 			-F "message=${comment_compose_t}" \
 			-F "source=@thumb.jpg" \
 			-o /dev/null \
-		"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || { [ -n "${post_id}" ] && printf '%s\n' "${post_id}" >> log.txt ; exit 1 ;}
+		"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || exit_custom
 	else
 		curl -sLf -X POST \
 			--data-urlencode "message=${comment_compose}" \
 			-o /dev/null \
-		"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || { [ -n "${post_id}" ] && printf '%s\n' "${post_id}" >> log.txt ; exit 1 ;}
+		"${graph_url_main}/v16.0/${id_post}/comments?access_token=${token}" || exit_custom
 	fi
 	
 	# comment some of the commentors
@@ -114,7 +124,10 @@ post_to_timeline(){
 	[ -e "thumb.jpg" ] && rm thumb.jpg
 	[ -e "vid.mp4" ] && rm vid.mp4
 	[ -e "com_tmp.jpg" ] && rm com_tmp.jpg
-	printf '%s\n' "${post_id}" >> log.txt
+	if [ -n "${post_id}" ]; then
+		fetch_gist_tofile+=$'\n'"${post_id}"
+		printf '%s' "${fetch_gist_tofile}" | jq --raw-input --slurp '{files: {myfile: {content: .}}}' | curl -X PATCH -sL "https://api.github.com/gists/226f8ed0960e64fc43f6c3aae4cadbe7" -H 'Accept: application/vnd.github.v3+json' -H "Authorization: token ${git_tok}" --data @-
+	fi
 }
 
 
@@ -124,7 +137,7 @@ while true; do
 
 	encoded="$(curl -sLk "https://mtouch.facebook.com/groups/${rand_gr}" -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36' -H 'sec-fetch-mode: navigate' -H 'sec-fetch-site: none' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' -H 'Accept-language: en-US,en;q=0.9' | sed -E 's|data-gt="\&\#123\;\&quot\;tn\&quot\;:\&quot\;C\&quot\;\&\#125\;\"|#flag#|g;s|amp;||g;;s|&quot;|"|g;s|\&#039;|\x27|g;s|\\([[:xdigit:]]{2}) |\\x\1|g;s_%([[:xdigit:]]{2})_\\x\1_g' | grep -o -P -- "#flag#(.*?)data-sigil=\"share-popup\">Share" | sed '1d' | awk 'BEGIN{srand();while(getline){a[++n]=$0}for(i=n;i;i--){j=int(rand()*i)+1;t=a[i];a[i]=a[j];a[j]=t}}END{for(i=1;i<=n;i++)print a[i]}' | shuf -n 1)"
 
-	[ -z "${encoded}" ] && exit 1
+	[ -z "${encoded}" ] && exit_custom
 	[ -e "log.txt" ] || : > log.txt
 	user="$(printf '%s' "${encoded}" | sed -nE 's_.*#flag#><span><strong><a[^>]*>([^<]*)<.*_\1_p')"
 	thumbnail="$(printf '%s' "${encoded}" | tr -dc '[:print:]' | sed -nE 's_\\\\_\\_g;'"s|.*url\('(.*n.jpg[^']*)'.*|\1|p" | sed '/.png?/ s_.*__g')"
@@ -136,7 +149,7 @@ while true; do
 	post_loc="$(printf '%s' "${encoded}" | sed -nE 's_.*"(.*/permalink/[^/]*)/.*_\1_p')"
 	[[ "${post_loc}" =~ https:// ]] || post_loc="https://www.facebook.com${post_loc}"
 	post_id="${post_loc##*/}"
-	[[ -z "${post_id}" ]] && exit 1 ; [[ -z "${post_loc}" ]] && exit 1
+	[[ -z "${post_id}" ]] && exit_custom ; [[ -z "${post_loc}" ]] && exit_custom
 	if grep -q "${post_id}" log.txt; then
 		unset rand_gr ids_arr encoded user thumbnail vid_link post_loc post_id
 		continue
@@ -147,15 +160,15 @@ done
 
 
 # some infos
-body="$(curl -sLkf "${post_loc}" -H "cookie:locale=en_US" -A "Mozilla/5.0 (Linux; Android 8.1.0; vivo 1801) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36" -H "sec-fetch-mode: navigate" -H "sec-fetch-site: none" -H "cookie:sb=xs" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Accept-language: en-US,en;q=0.9")" || exit 1
-
+fetch_gist_tofile="$(curl -sLk "${fetch_gist_base}" | tee log.txt)" || { echo "Failed to Reach logfile" ; exit_custom ;}
+body="$(curl -sLkf "${post_loc}" -H "cookie:locale=en_US" -A "Mozilla/5.0 (Linux; Android 8.1.0; vivo 1801) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36" -H "sec-fetch-mode: navigate" -H "sec-fetch-site: none" -H "cookie:sb=xs" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9" -H "Accept-language: en-US,en;q=0.9")" || exit_custom
 status="$(printf '%s' "${body}" | sed -nE 's|.*comment_count":"([^"]*)".*reaction_count":\{"count":([^\}]*)\}.*i18n_share_count":"([^"]*)".*|\2#\1#\3|p')"
 status_footer="$(printf '%s' "${body}" | grep -oP '"name":"[^"]*","__isActor":"User","(.*?)is_markdown_enabled')"
 
 likes="$(printf '%s' "${status}" | cut -d'#' -f1)"
 comments="$(printf '%s' "${status}" | cut -d'#' -f2)"
 shares="$(printf '%s' "${status}" | cut -d'#' -f3)"
-date_posted="$(sed -nE 's|.*creation_time":([^,]*).*|\1|p' <<< "${body}" | head -n 1 | grep '[^[:space:]]')" || exit 1
+date_posted="$(sed -nE 's|.*creation_time":([^,]*).*|\1|p' <<< "${body}" | head -n 1 | grep '[^[:space:]]')" || exit_custom
 date_posted="$(date +"%b %d, %+4Y at %r (%Z)" -d @"${date_posted}")"
 caption="{\"data\":\"$(printf '%s' "${body}" | sed -nE 's|.*"message":\{"text":"([^"]*)".*|\1|p')\"}"
 # Decoding unicodes using jq
@@ -163,7 +176,7 @@ caption="$(printf '%s' "$caption" | jq -r .data)"
 
 # check profane
 [ -n "${caption}" ] && caption="$(curl -s -X POST -H "Content-Type:application/x-www-form-urlencoded; charset=UTF-8" -H "X-Requested-With:XMLHttpRequest" -H "sec-ch-ua-mobile:?1" -H "User-Agent:Mozilla/5.0 (Linux; Android 8.1.0; vivo 1801) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36" -H "Origin:https://app.readable.com" -H "Sec-Fetch-Site:same-origin" -H "Sec-Fetch-Mode:cors" -H "Sec-Fetch-Dest:empty" -H "Referer:https://app.readable.com/text/profanity/" -d "type=text&batch%5B0%5D%5Btext%5D=$(sed 's|+|%2B|g;s| |+|g;s|"|%22|g;s|\x27|%27|g;s|\\|%5C|g' <<< "${caption}")&list=profanity" "https://app.readable.com/live/wordlist" | jq -r .items[].highlighted_text | sed -E 's|<span[^>]*>(.{2})([^>]*)<[^>]*>|\1**\2|g')"
-group_name="$(printf '%s' "${body}" | sed -nE 's|.*"group":\{"name":"([^"]*)".*|\1|p' | grep '[^[:space:]]')" || exit 1
+group_name="$(printf '%s' "${body}" | sed -nE 's|.*"group":\{"name":"([^"]*)".*|\1|p' | grep '[^[:space:]]')" || exit_custom
 
 post_to_timeline
 unset user thumbnail vid_link post_loc post_id caption likes comments shares date_posted body status status_footer usr_com com_capt comnts imglnk date_crcm react_tcom r_m d_m
