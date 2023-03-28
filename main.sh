c18a6aa0dc9e8127713a7db58fa01a0c1a6a5c08
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-03-28 14:42:25 +0900

    fix: slow

--- main.sh
+++ main.sh
@@ -3,9 +3,9 @@
 posting="$(shuf -n 1 -i 8-12)"
 i=0
 while [[ "$((i+=1))" -le "${posting}" ]]; do
+    [[ "${rep_its:-0}" -ge 5 ]] && { echo "Failed 5 times" ; break ;}
     bash fblowres.sh "${1}" "${2}" || { : "$((rep_its+=1))" ; echo "error: retrying" ; continue ;}
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
-    [[ "${rep_its}" -ge 5 ]] && { echo "Failed 5 times" ; break ;}
 done
 # Update about
 total_archived="$(curl -sL "https://gist.githubusercontent.com/Veroniclover/226f8ed0960e64fc43f6c3aae4cadbe7/raw/myfile" | wc -l) posts has been successfully backed up from March 2023 until now"
