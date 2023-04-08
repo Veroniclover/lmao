90aea0874448ac8afb0d558f671f0a006d58c22b
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-04-08 10:00:53 +0900

    feat: automate current month

--- main.sh
+++ main.sh
@@ -8,5 +8,5 @@ while [[ "$((i+=1))" -le "${posting}" ]]; do
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
 done
 # Update about
-total_archived="$(curl -sL "https://gist.githubusercontent.com/Veroniclover/226f8ed0960e64fc43f6c3aae4cadbe7/raw/myfile" | wc -l) posts has been successfully backed up from March 2023 until now"
+total_archived="$(curl -sL "https://gist.githubusercontent.com/Veroniclover/226f8ed0960e64fc43f6c3aae4cadbe7/raw/myfile" | wc -l) posts has been successfully backed up from $(date +%B) 2023 until now"
 curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${total_archived}" || true
