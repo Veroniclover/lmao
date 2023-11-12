7c744c81529038285d459e89294eea7dfe302d3b
Veroni <73184907+Veroniclover@users.noreply.github.com>
2023-11-12 12:15:33 +0900

    he

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-posting="$(shuf -n 1 -i 8-12)"
+posting="$(shuf -n 1 -i 10-14)"
 i=0
 while [[ "$((i+=1))" -le "${posting}" ]]; do
     [[ "${rep_its:-0}" -ge 5 ]] && { echo "Failed 5 times" ; break ;}
@@ -8,5 +8,5 @@ while [[ "$((i+=1))" -le "${posting}" ]]; do
     sleep "$(awk -v "a=120" -v "b=160" -v "c=$RANDOM" 'BEGIN{srand(c);print int(a+rand()*(b-a+1))}')"
 done
 # Update about
-total_archived="$(curl -sL "https://gist.githubusercontent.com/Veroniclover/226f8ed0960e64fc43f6c3aae4cadbe7/raw/myfile" | wc -l) posts has been successfully backed up from $(date +%B) 2023 until now"
+total_archived="$(curl -sL "https://gist.githubusercontent.com/Veroniclover/226f8ed0960e64fc43f6c3aae4cadbe7/raw/myfile" | wc -l) posts has been successfully backed up as of $(date +"%B %Y")"
 curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${total_archived}" || true
